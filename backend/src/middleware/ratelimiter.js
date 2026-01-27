bf3eee668ecb26fe343efd5e13aba846cd734a1a
Muqeet-Ryan <muqeetryan@outlook.com>
2026-01-27 16:58:58 +0600

    added ratelimiter on routes

---
+++ backend/src/middleware/ratelimiter.js
@@ -0,0 +1,15 @@
+import ratelimit from "../config/upstash.js";
+
+
+const rateLimiter = async(req,res,next) => {
+    try {
+        const {success} = await ratelimit.limit('my-limit-key');
+        if(!success) return res.status(429).json({msg: 'too many request'});
+
+        next();
+    } catch (error) {
+        next(error);
+    }
+}
+
+export default rateLimiter;
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,12 +2,15 @@ import express from 'express';
 import notesRoute from './routes/notesRoutes.js';
 import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
+import rateLimiter from './middleware/ratelimiter.js';
 dotenv.config();
 
 const app = express();
 
 connectDB();
 app.use(express.json());
+
+app.use(rateLimiter);
 app.use('/api/notes', notesRoute);
 
 const PORT = process.env.PORT; 
